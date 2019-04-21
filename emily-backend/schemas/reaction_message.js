a8c988f3181669a2371758a862c50d1a85430ab1
Yannis Li <yannislightli@gmail.com>
2019-04-21 05:25:08 -0400

    Changed schema

--- emily-backend/schemas/reaction_message.js
+++ emily-backend/schemas/reaction_message.js
@@ -3,17 +3,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 
-let roleSchema = mongoose.Schema({
-    emoji: String,
-    role: String
-});
 const MessageSchema = new Schema(
-{
-    channel: String,
-    message: String,
-    guild: String,
-    reactions: [roleSchema]
-});
-    
+    {
+        channel: String,
+        message: String,
+        guild: String,
+        reactions: 
+        {
+            type: [
+            {
+                emoji: String,
+                role: String,
+            }
+            ],
+            default: []
+        }
+    });
     
 module.exports = mongoose.model("Message", MessageSchema);
